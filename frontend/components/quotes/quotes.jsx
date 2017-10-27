b5dd90b2c777cd43878fe00061fc40ee5acbc7d7
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2017-10-27 17:34:43 -0400

    Implement quote rendering

--- frontend/components/quotes/quotes.jsx
+++ frontend/components/quotes/quotes.jsx
@@ -6,30 +6,49 @@ class QuotePage extends React.Component {
     super(props);
   }
 
-  render() {
-    return (
-      <div id="quote">
-        <h2>
-          Quotes
-        </h2>
-        <div>
-          <div id="quote-text">
+  componentDidMount() {
+    this.props.fetchQuotes();
+  }
 
-          </div>
-          <div className="quote-links">
-            <Link to="/construction">Best Quotes</Link>
-            <Link to="/construction">Love Quotes</Link>
-            <Link to="/construction">Inspirational Quotes</Link>
-            <Link to="/construction">Funny Quotes</Link>
-            <Link to="/construction">Motivational Quotes</Link>
-            <Link to="/construction">Life Quotes</Link>
-            <Link to="/construction">Friends Quotes</Link>
-            <Link to="/construction">Positive Quotes</Link>
-            <Link to="/construction">More Quotes</Link>
+  render() {
+    if (this.props.quotes[1]) {
+      return (
+        <div id="quote">
+          <h2>
+            Quotes
+          </h2>
+          <div>
+            <div id="quote-text">
+              <div className="portrait">
+
+              </div>
+              <div className="quote-content">
+                <div>
+                  { this.props.quotes[1].content }
+                </div>
+                <div>
+                  { this.props.quotes[1].author }
+                </div>
+              </div>
+            </div>
+            <div className="quote-links">
+              <Link to="/construction">Best Quotes</Link>
+              <Link to="/construction">Love Quotes</Link>
+              <Link to="/construction">Inspirational Quotes</Link>
+              <Link to="/construction">Funny Quotes</Link>
+              <Link to="/construction">Motivational Quotes</Link>
+              <Link to="/construction">Life Quotes</Link>
+              <Link to="/construction">Friends Quotes</Link>
+              <Link to="/construction">Positive Quotes</Link>
+              <Link to="/construction">More Quotes</Link>
+            </div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
+    else {
+      return (<div></div>);
+    }
   }
 }
 
--- frontend/components/quotes/quotes_container.jsx
+++ frontend/components/quotes/quotes_container.jsx
@@ -1,12 +1,14 @@
 import { connect } from 'react-redux';
 import QuotePage from './quotes';
+import { fetchQuotes, fetchQuote } from '../../actions/quote_actions';
 
 const mapStateToProps = (state, ownProps) => ({
-
+  quotes: state.quotes,
 });
 
-const mapDispatchToProps = (state, ownProps) => ({
-
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  fetchQuotes: () => dispatch(fetchQuotes()),
+  fetchQuote: (quoteId) => dispatch(fetchQuote(quoteId)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(QuotePage);
