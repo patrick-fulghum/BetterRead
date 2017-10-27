e93353e3f4b8948539471a75aa613f84729adc9b
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2017-10-27 12:28:06 -0400

    Write and style discovery react component

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -5,10 +5,12 @@ import SessionFormContainer from './sessions/session_form_container';
 import LandingPageContainer from './landing/landing_page_container';
 import AuthRoute from '../api_util/route_util';
 import Construct from './construction/construction';
+import DiscoveryPageContainer from './discovery/discovery_container';
 
 const App = () => (
   <div id="ultimate-container">
     <Route exact path="/" component={LandingPageContainer} />
+    <Route exact path="/" component={DiscoveryPageContainer} />
     <Route path="/construction" component={Construct} />
     <AuthRoute path="/signup" component={SessionFormContainer} />
     <AuthRoute path="/login" component={SessionFormContainer} />
---
+++ frontend/components/discovery/discovery.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link, withRouter } from 'react-router-dom';
+
+class DiscoveryPage extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(e) {
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <div id="discovery">
+        <h2>
+          What will <i>you</i> discover?
+        </h2>
+        <div className="discovery-main">
+          <div className="discovery-p-tags">
+            <p>Because Guest liked...</p>
+            <p>They Discovered:</p>
+          </div>
+          <ul className="book-listing">
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+            <div className="arrow-box">
+              <i className="fa fa-arrow-right"></i>
+            </div>
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+            <li className="hp-image">
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DiscoveryPage;
