404822691324baae257dee58272b269b8c3e60bc
Patrick Fulghum <patrick.c.fulghum@gmail.com>
2017-11-02 13:46:15 -0400

    Actions and api for reviews

--- frontend/actions/review_actions.jsx
+++ frontend/actions/review_actions.jsx
@@ -7,3 +7,23 @@ const receiveReview = review => ({
   type: RECEIVE_REVIEW,
   review,
 });
+
+const destroyReview = reviewId => ({
+  type: DELETE_REVIEW,
+  reviewId
+});
+
+export const createReview = (bookId, thisReview) => dispatch => (
+  APIUtil.createReview(bookId, thisReview)
+  .then(review => dispatch(receiveReview(review)))
+);
+
+export const updateReview = (bookId, thisReview) => dispatch => (
+  APIUtil.updateReview(bookId, thisReview)
+  .then(review => dispatch(receiveReview(review)))
+);
+
+export const deleteReview = (bookId, reviewId) => dispatch => (
+  APIUtil.deleteReview(bookId, reviewId)
+  .then(review => dispatch(destroyReview(review)))
+);
